2aef5388749948a230e46e1d922673b6154c2b9b
Michael Westerby <michael.westerby@ibm.com>
2020-10-14 19:52:37 +0100

    Navbar improvements and layout refactoring

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import styles from '../styles/layout.module.css'
 import Footer from './footer'
 import Navbar from './navbar'
+import Banner from './banner'
 
 const name = 'Michael Westerby'
 export const siteTitle = name
@@ -11,7 +10,7 @@ export const siteTitle = name
 export default function Layout({ children, home }) {
   return (
     <>
-      <Navbar />
+      <Navbar home={home}/>
       <div className={styles.container}>
         <Head>
           <link rel="icon" href="/favicon.ico" />
@@ -19,42 +18,9 @@ export default function Layout({ children, home }) {
           <meta name="og:title" content={siteTitle} />
         </Head>
         <header className={styles.header}>
-          {home ? (
-            <>
-              <img
-                src="/images/profile.jpg"
-                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                alt={name}
-              />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <a>
-                  <img
-                    src="/images/profile.jpg"
-                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                    alt={name}
-                  />
-                </a>
-              </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/">
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-            </>
-          )}
+          <Banner home={home} name={name}/>
         </header>
         <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">
-              <a>← Back to home</a>
-            </Link>
-          </div>
-        )}
         <Footer />
       </div>
     </>
