a1b214734fd026fae98d46bffdd30bfe64769bb6
Michael Westerby <michael.westerby@ibm.com>
2020-11-14 00:21:21 +0000

    Adding current progress

---
+++ content/projects/call-for-code.md
@@ -0,0 +1,8 @@
+---
+title: 'Project Reunite'
+startDate: '2019-06-01'
+endDate: '2020-09-30'
+link: 'https://github.com/project-reunite/reunite'
+---
+
+I, along with four other graduates took part in IBM’s Call for Code hackathon, an event focused on designing technical solutions to help victims of natural disasters. Here I was responsible for designing and developing part of a React front end, alongside promotional videos, for an application to help reunite lost loved ones. Our solution was well received and placed us in the top five of the world-wide competition out of 138 teams and subsequently demoed at IBM’s Think Summit event in London.
--- pages/cv.js
+++ pages/cv.js
@@ -1,6 +1,7 @@
 import Layout, {siteTitle} from '../components/layout'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
+import cvStyles from '../styles/cv.module.css'
 import { getCV } from '../lib/cv'
 
 export default function CV({cvData}) {
@@ -16,7 +17,38 @@ export default function CV({cvData}) {
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h1 className={utilStyles.headingXL}>CV</h1>
-        <div dangerouslySetInnerHTML={{ __html: cvData.contentHtml }} />
+
+        <h1>Experience</h1>
+          <h2>IBM</h2>
+          <h3 className={cvStyles.subheading}>Software Engineer - CICS</h3>
+          <p className={cvStyles.date}>September 2020 - Present</p>
+          <p>Software developer and tester for the <a href='https://www.ibm.com/uk-en/products/cics-transaction-server'>CICS Transaction Server </a>on z/OS.</p>
+          
+          <h3 className={cvStyles.subheading}>Software Engineer - API Connect</h3>
+          <p className={cvStyles.date}>September 2018 - September 2020</p>
+          <p>Full stack developer in IBM Cloud, responsible for the software development behind IBM's API lifecycle management program, <a href='https://www.ibm.com/uk-en/cloud/api-connect'>API Connect</a>.</p>        
+
+        <h1>Education</h1>
+          <h2>Newcastle University</h2>
+          <h3 className={cvStyles.subheading}>MSc Computer Science, Distinction</h3>
+          <p className={cvStyles.date}>September 2017 - August 2018</p>
+          <p>Masters Project: A Peer and Self-Assessment Tool for Software Engineering Teams</p>
+
+          <h2>Durham University</h2>
+          <h3 className={cvStyles.subheading}>BSc Geography, First Class Honors</h3>
+          <p className={cvStyles.date}>September 2014 - June 2017</p>
+          <p>Dissertation: An Investigation into the use of Structure-from-Motion Photogrammetry in Reconstructing Flood Extents from Smartphone Imagery.</p>
+
+        <h1>Technical Skills</h1>
+          <h3 className={cvStyles.subheading}>Languages</h3>
+          <p>JavaScript, Golang, Java</p>
+
+          <h3 className={cvStyles.subheading}>Frameworks / Tools</h3>
+          <p>React, Next.js, HTML/CSS</p>
+
+          <h3 className={cvStyles.subheading}>Devops / SCM</h3>
+          <p>Docker, Kubernetes, Jenkins, Git, Rational Team Concert</p>
+        {/* <div dangerouslySetInnerHTML={{ __html: cvData.contentHtml }} /> */}
       </section>
     </Layout>
   )
