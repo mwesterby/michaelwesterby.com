339e9442643d7c06a3bef3bc8d96c710fc96d596
Michael Westerby <48826192+mwesterby@users.noreply.github.com>
2021-08-11 13:47:46 +0100

    Refactored Nav bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -5,18 +5,38 @@ import styled from '@emotion/styled'
 import DarkModeSwitch from './DarkModeSwitch'
 import { useRouter } from 'next/router'
 
-const NavigationBar = (): JSX.Element => {
+const NavigationBarItem = ({ path, name }): JSX.Element => {
 	const { colorMode } = useColorMode()
 	const router = useRouter()
-
-	const bgColor = {
-		light: 'white',
-		dark: '#171717',
-	}
 
 	const navHoverBg = {
 		light: 'gray.100',
 		dark: 'gray.700',
+	}
+
+	return (
+		<NextLink href={path} passHref>
+			<Button
+				as="a"
+				variant="ghost"
+				p={[1, 2, 4]}
+				mr={1}
+				backgroundColor={
+					router.pathname === path ? navHoverBg[colorMode] : null
+				}
+			>
+				{name}
+			</Button>
+		</NextLink>
+	)
+}
+
+const NavigationBar = (): JSX.Element => {
+	const { colorMode } = useColorMode()
+
+	const bgColor = {
+		light: 'white',
+		dark: '#171717',
 	}
 
 	const StickNav = styled(Flex)`
@@ -44,65 +64,10 @@ const NavigationBar = (): JSX.Element => {
 			mx="auto"
 		>
 			<Box>
-				<NextLink href="/" passHref>
-					<Button
-						as="a"
-						variant="ghost"
-						p={[1, 2, 4]}
-						mr={1}
-						backgroundColor={
-							router.pathname === '/'
-								? navHoverBg[colorMode]
-								: null
-						}
-					>
-						Home
-					</Button>
-				</NextLink>
-				<NextLink href="/projects" passHref>
-					<Button
-						as="a"
-						variant="ghost"
-						p={[1, 2, 4]}
-						mr={1}
-						backgroundColor={
-							router.pathname === '/projects'
-								? navHoverBg[colorMode]
-								: null
-						}
-					>
-						Projects
-					</Button>
-				</NextLink>
-				<NextLink href="/blog" passHref>
-					<Button
-						as="a"
-						variant="ghost"
-						p={[1, 2, 4]}
-						mr={1}
-						backgroundColor={
-							router.pathname === '/blog'
-								? navHoverBg[colorMode]
-								: null
-						}
-					>
-						Blog
-					</Button>
-				</NextLink>
-				<NextLink href="/cv" passHref>
-					<Button
-						as="a"
-						variant="ghost"
-						p={[1, 2, 4]}
-						backgroundColor={
-							router.pathname === '/cv'
-								? navHoverBg[colorMode]
-								: null
-						}
-					>
-						CV
-					</Button>
-				</NextLink>
+				<NavigationBarItem path="/" name="Home" />
+				<NavigationBarItem path="/projects" name="Projects" />
+				<NavigationBarItem path="/blog" name="Blog" />
+				<NavigationBarItem path="/cv" name="CV" />
 			</Box>
 			<DarkModeSwitch />
 		</StickNav>
